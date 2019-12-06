fb5469168267b89da9893663bc8d07c4b492f381
mickh14 <michael.hurley@expleogroup.com>
2019-12-06 08:49:29 +0000

    update run with basic views and routes

--- run.py
+++ run.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<H1>Hello there</H1>"
+    """Main Page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
+
+@app.route('/<username>')
+def user(username):
+    return "Hi " + username
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')),debug=True)        
